ff67616dda488cf6176b7ca8ebb4fc71e63926ec
amankshah <kumarshahaman@gmail.com>
2023-10-15 17:33:21 +0530

    Connect To Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
-import  express  from 'express';
+import  dotenv  from 'dotenv';
 
+import  express  from 'express';
+import mongoose from 'mongoose';
+
+let  MongoUrl = "mongodb+srv://Aman:Aman@real-estate.uh7pqry.mongodb.net/real-estate?retryWrites=true&w=majority";
+
+dotenv.config();
+console.log("env value :" ,process.env.MONGO);
+
+// mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(MongoUrl).then(()=>{
+    console.log('Connected to MongoDB!!');
+    
+}).catch((err)=> {
+    console.error("Error connecting to the database", err);
+});
 const app = express();
 
 app.listen(3000, () => {
