9ca9fedb110c08acb9b596d21604a9fec3a89a0f
amankshah <kumarshahaman@gmail.com>
2023-10-18 01:12:34 +0530

    Created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,61 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import Jwt from "jsonwebtoken";
 
+let JWT_sec = process.env.JWT_SECRET;
+// console.log(JWT_SECRET);
 
-export const signup = async(req, resp, next)=>{
-// console.log(req.body);
-const {username, email , password } = req.body;
-const hashedPassword = bcryptjs.hashSync(password,10);
-const newUser = new User({username , email, password:hashedPassword});
-try{
+export const signup = async (req, resp, next) => {
+  // console.log(req.body);
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    resp.status(201).json('User Created Successfully!');
+    resp.status(201).json("User Created Successfully!");
+  } catch (error) {
+    next(errorHandler(550, "Error from a function"));
+  }
+};
 
-}catch(error){
-    next(errorHandler(550, 'Error from a function'))
-}
+// export const signin = async (req, resp, next) => {
+//   // console.log(req.body);
+//   const { email, password } = req.body;
 
+//   try {
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) return next(errorHandler(404, "Invalid Email ")); //or Password
+//     const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+//     if (!isValidPassword) return next(errorHandler(401, "Wrong Credentials!"));
+//     const token = Jwt.sign({ id: validUser._id }, JWT_SECRET);
+//     const { password: pass, ...rest } = validUser;
+//     resp
+//       .cookie("access_Token", token, { httpOnly: true })
+//       .sendStatus(200)
+//       .json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = Jwt.sign({ id: validUser._id }, JWT_sec);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,44 +1,44 @@
-import  dotenv  from 'dotenv';
-
-import  express  from 'express';
-import mongoose from 'mongoose';
-
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-
-
-let  MongoUrl = "mongodb+srv://Aman:Aman@real-estate.uh7pqry.mongodb.net/real-estate?retryWrites=true&w=majority";
-let port =3001
+import dotenv from "dotenv";
 dotenv.config();
-console.log("env value :" ,process.env.MONGO);
+
+import express from "express";
+import mongoose from "mongoose";
+
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+let MongoUrl = process.env.MONGO_URL;
+let port = 3001;
+
+// console.log("env value :", process.env.MONGO_URL);
 
 // mongoose.connect(process.env.MONGO).then(()=>{
-mongoose.connect(MongoUrl).then(()=>{
-    console.log('Connected to MongoDB!!');
-    
-}).catch((err)=> {
+mongoose
+  .connect(MongoUrl)
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
     console.error("Error connecting to the database", err);
-});
+  });
 const app = express();
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log('Server is running on port',port);
-    
+  console.log("Server is running on port", port);
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 //Middleware
 
-app.use((err, req, resp, next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return resp.status(statusCode).json({
-        success :false,
-        statusCode,
-        message,
-    })
-})
+app.use((err, req, resp, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return resp.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
