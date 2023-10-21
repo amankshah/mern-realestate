34966ea43ef3cd086654dc3445d51d950c3addfd
amankshah <kumarshahaman@gmail.com>
2023-10-21 14:48:53 +0530

    Added avatar in header and made profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 // import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -27,16 +30,26 @@ export default function Header() {
               Home
             </li>
           </Link>
-
           <Link to="/about">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
-
-          <Link to="/sign-in">
-            <li className=" text-slate-700 hover:underline">SignIn</li>
-          </Link>
+          {/* //checking the condition if current user exist then  */}
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="Profile Image"
+                srcset=""
+              />
+            </Link>
+          ) : (
+            <Link to="/sign-in">
+              <li className=" text-slate-700 hover:underline">SignIn</li>
+            </Link>
+          )}
         </ul>
       </div>
     </header>
