1a150b80ff172692e386f447ce1e4e9a0be3f165
amankshah <kumarshahaman@gmail.com>
2023-10-22 13:01:31 +0530

    Configured Local host to see in all devices in local network

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3001',
-        secure:false,
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        secure: false,
       },
     },
   },
